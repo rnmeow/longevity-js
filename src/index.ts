9309b54a2d61719e8c5903c5a8f1965a7db80940
Yu-huan Kuo <touch@rnmeow.com>
2025-02-14 21:46:15 +0800

    perf: simplify calculation logic

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,34 @@
 const isLeapYear = (ceYear: number): boolean =>
-  ceYear % 100 !== 0 || (ceYear % 100 === 0 && ceYear % 400 === 0)
+  ceYear % 100 !== 0 || ceYear % 400 === 0
 
 const getMsOfYear = (ceYear: number): number =>
   isLeapYear(ceYear) ? 31622400000 : 31536000000
 
 export function calcAge(birthday: Date): number {
-  const now = new Date()
+  const now = new Date(),
+    thisYear = now.getUTCFullYear()
 
-  const endOfBirthYear = new Date(
-      Date.UTC(birthday.getUTCFullYear(), 11, 31, 23, 59, 59, 999),
-    ),
-    birthYearLeftMs = endOfBirthYear.getTime() - birthday.getTime(),
-    birthYearTotalMs = getMsOfYear(birthday.getUTCFullYear())
+  const birthYear = birthday.getUTCFullYear()
 
-  const startOfThisYear = new Date(
-      Date.UTC(now.getUTCFullYear(), 0, 1, 0, 0, 0, 0),
-    ),
-    thisYearPassedMs = now.getTime() - startOfThisYear.getTime(),
-    thisYearTotalMs = getMsOfYear(now.getUTCFullYear())
+  const startOfThisYear = new Date(Date.UTC(thisYear, 0, 1, 0, 0, 0, 0))
+  const thisYearPassedMs = now.getTime() - startOfThisYear.getTime()
+  const thisYearTotalMs = getMsOfYear(thisYear)
+
+  const endOfBirthYear = new Date(Date.UTC(birthYear, 11, 31, 23, 59, 59, 999))
+  const birthYearLeftMs = endOfBirthYear.getTime() - birthday.getTime()
+  const birthYearTotalMs = getMsOfYear(birthYear)
 
   const ageYears =
     // leftover days in the year of birth
     birthYearLeftMs / birthYearTotalMs +
     // passed years
-    now.getUTCFullYear() -
-    birthday.getUTCFullYear() +
+    thisYear -
+    (birthYear + 1) +
     // days from this year
-    thisYearPassedMs / thisYearTotalMs -
-    // shísuì
-    1
+    thisYearPassedMs / thisYearTotalMs
 
   /*
-   * Copyright (C) 2024 Connor Kuo, licensed under MIT.
+   * Copyright (C) 2024, 2025, Yu-huan Kuo. Licensed under MIT.
    */
 
   return ageYears
