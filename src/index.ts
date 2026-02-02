a8fedc03128b080b8120d3d3a6c520b9dca330e3
Connor Kuo <git@rnmeow.com>
2026-02-02 18:09:06 +0800

    fix: potential timezone and off-by-one error

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,12 @@ export function calcAge(birthday: Date): number {
 
   const age =
     // leftover days in the year of birth
-    (+new Date(Date.UTC(birthYear, 11, 31, 23, 59, 59, 999)) -
-      birthday.getTime()) /
+    (Date.UTC(birthYear, 11, 31, 23, 59, 59, 999) - birthday.getTime()) /
       getMsOfYear(birthYear) +
     // passed years
-    thisYear -
-    (birthYear + 1) +
+    Math.max(0, thisYear - birthYear - 1) +
     // days from this year
-    (now.getTime() - +new Date(Date.UTC(thisYear, 0, 1, 0, 0, 0, 0))) /
+    (now.getTime() - Date.UTC(thisYear, 0, 1, 0, 0, 0, 0)) /
       getMsOfYear(thisYear)
 
   /*
