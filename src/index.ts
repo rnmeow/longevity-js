2bd1b1c763f07f11ff71c692c121673e83abd836
Connor Kuo <git@rnmeow.com>
2026-02-02 18:12:36 +0800

    refactor: fix grammar mistakes in naming

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
 const isLeapYear = (ceYear: number): boolean =>
   ceYear % 4 === 0 && (ceYear % 100 !== 0 || ceYear % 400 === 0)
 
-const getMsOfYear = (ceYear: number): number =>
+const getMsInYear = (ceYear: number): number =>
   isLeapYear(ceYear) ? 31622400000 : 31536000000
 
 export function calcAge(birthday: Date): number {
   const now = new Date(),
-    thisYear = now.getUTCFullYear(),
+    curYear = now.getUTCFullYear(),
     birthYear = birthday.getUTCFullYear()
 
   const age =
     // leftover days in the year of birth
     (Date.UTC(birthYear, 11, 31, 23, 59, 59, 999) - birthday.getTime()) /
-      getMsOfYear(birthYear) +
+      getMsInYear(birthYear) +
     // passed years
-    Math.max(0, thisYear - birthYear - 1) +
+    Math.max(0, curYear - birthYear - 1) +
     // days from this year
-    (now.getTime() - Date.UTC(thisYear, 0, 1, 0, 0, 0, 0)) /
-      getMsOfYear(thisYear)
+    (now.getTime() - Date.UTC(curYear, 0, 1, 0, 0, 0, 0)) / getMsInYear(curYear)
 
   /*
    * Copyright (C) 2024, 2025, Yu-huan Kuo. Licensed under MIT.
